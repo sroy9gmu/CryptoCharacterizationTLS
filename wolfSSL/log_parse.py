86993026fe6155b04f5ba15bda4b383b741d8fec
Sunanda Roy <sroy9@gmu.edu>
2025-05-27 17:05:37 -0400

    fas

--- wolfSSL/log_parse.py
+++ wolfSSL/log_parse.py
@@ -107,10 +107,10 @@ def main(infile, outfile):
         f.write(f"Key Exchange: {kx_algo_g}\n")
         f.write(f"Signature: {sg_algo_g}\n")
         f.write(f"Encryption and Hashing: {en_dg_algo_g}\n\n")
-        f.write(f"Total execution time of all direct invocations: {total_dur}\n")
+        f.write(f"Total execution time of all direct invocations: {"{:,.2f}".format(total_dur)}\n")
         f.write("Breakdown of total execution time (direct invocation).\n")
         for index, (key, value) in enumerate(times_sum.items()):
-            f.write(f"Function name: {key}, time (microseconds): {value}\n")
+            f.write(f"Function name: {key}, time (microseconds): {"{:,.2f}".format(value)}\n")
 
         f.write("\n**************Debug**************\n")
         f.write(f"Key Exchange: \n")
