ae6243b907602b38279e892ea9a53db6a0e97d88
Sunanda Roy <sroy9@gmu.edu>
2025-07-10 11:10:16 -0400

    sdf

--- plots/time_ffdh2048_rsa3072_aes128gcm_sha256.py
+++ plots/time_ffdh2048_rsa3072_aes128gcm_sha256.py
@@ -7,31 +7,39 @@
 # rows = ['OpenSSL', 'wolfSSL', 'MbedTLS', 'GnuTLS']
 
 # data from https://allisonhorst.github.io/palmerpenguins/
-
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-algos = ("FFDH-2048", "RSA-PSS-3072", "AES-128-GCM", "SHA-256", "Total")
-times = {
-    'OpenSSL': (1725770.64, 34664.27, 58.0, 16.156222),
-    'wolfSSL': (38191.548417, 35588.0, 66.0, 161.0),
-    'MbedTLS': (94037.0, 42559.0, 45.0, 76.0),
-    'GnuTLS': (52094.0, 54453.0, 86.0, 167.0)
-}
-
+# algos = ("FFDH-2048", "RSA-PSS-3072", "AES-128-GCM", "SHA-256", "Total")
+# times = {
+#     'OpenSSL': (1688307, 34664.3, 58, 16.1),
+#     'wolfSSL': (38191.5, 35588, 66, 161),
+#     'MbedTLS': (94037, 42559, 45, 76),
+#     'GnuTLS': (52094, 54453, 86, 167)
+# }
+# print(matplotlib.__version__)
 libs = ('OpenSSL', 'wolfSSL', 'MbedTLS', 'GnuTLS')
+# durs = {
+#     # 'FFDH-2048': (1688307, 38191.5, 94037, 52094),
+#     'Key Exchange': (110000, 38191.5, 94037, 52094),
+#     'Signing': (34664.3, 35588, 42559, 54453),
+#     'Symmetric Encryption': (58, 66, 45, 86),
+#     'Hashing': (16.1, 161, 76, 167)
+# }
 durs = {
-    'FFDH-2048': (1688307.0, 38191.548417, 94037.0, 52094.0),
-    'RSA-PSS-3072': (34664.27, 35588.0, 42559.0, 54453.0),
-    'AES-128-GCM': (58.0, 66.0, 45.0, 86.0),
-    'SHA-256': (16.156222, 161.0, 76.0, 167.0)
+    'Hashing': (16.1, 161, 76, 167),
+    'Symmetric Encryption': (58, 66, 45, 86),
+    'Signing': (34664.3, 35588, 42559, 54453),
+    # 'FFDH-2048': (1688307, 38191.5, 94037, 52094),
+    'Key Exchange': (120000, 38191.5, 94037, 52094),   
 }
 
 bottom = np.zeros(3)
 x = np.arange(len(libs))  # the label locations
 width = 0.2  # the width of the bars
 
-fig, ax = plt.subplots(layout='tight')
+fig, ax = plt.subplots(layout='constrained')
 bottom = np.zeros(4)
 for boolean, dur in durs.items():
     p = ax.bar(libs, dur, width, label=boolean, bottom=bottom)
@@ -39,32 +47,35 @@ for boolean, dur in durs.items():
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
 ax.set_ylabel('Duration (microseconds)')
-ax.set_title('Execution time breadown')
+# ax.set_title('Execution time breadown')
 # ax.set_xticks(x + width, libs)
-ax.legend(loc='lower center', ncols=4)
-# ax.set_ylim(0, 100000)
-ax.set_yscale('log')
+ax.legend(loc=(1, 0.8))
+ylim = 0
+for index, (key, value) in enumerate(durs.items()):
+    ylim += max(durs[key])
+ax.set_ylim(0, ylim)
+# ax.set_yscale('log')
 ax.minorticks_off()
-
+# ax.invert_xaxis() 
 ax.grid(axis='y', linestyle='--') # Add horizontal grid lines
 
-data = []
-row_labels = []
-for index, (key, value) in enumerate(times.items()):
-    tmp = list(value)
-    for i in range(len(tmp)):
-        tmp[i] = "{:,.2f}".format(tmp[i]) # https://queirozf.com/entries/python-number-formatting-examples
-    tmp.append("{:,.2f}".format(sum(value)))
-    data.append(tmp)    
-    row_labels.append(key)
-col_labels = algos
+# data = []
+# row_labels = []
+# for index, (key, value) in enumerate(times.items()):
+#     tmp = list(value)
+#     for i in range(len(tmp)):
+#         tmp[i] = "{:,.2f}".format(tmp[i]) # https://queirozf.com/entries/python-number-formatting-examples
+#     tmp.append("{:,.2f}".format(sum(value)))
+#     data.append(tmp)    
+#     row_labels.append(key)
+# col_labels = algos
 
-fig, ax = plt.subplots()
-ax.axis('off')  # Hide the axes
-table = plt.table(cellText=data, colLabels=col_labels, rowLabels=row_labels, loc='center')
-table.scale(1, 1.5) # Adjust table size
+# fig, ax = plt.subplots()
+# ax.axis('off')  # Hide the axes
+# table = plt.table(cellText=data, colLabels=col_labels, rowLabels=row_labels, loc='center')
+# table.scale(1, 1.5) # Adjust table size
 
-table.auto_set_font_size(False) # Disable autosizing
+# table.auto_set_font_size(False) # Disable autosizing
 # table.set_fontsize(12) # Set font size
 
 plt.show()
