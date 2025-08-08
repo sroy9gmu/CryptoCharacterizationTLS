feb125d9e9be5219e21f4bf8cdf1b02a3b0d0139
Sunanda Roy <sroy9@gmu.edu>
2025-08-07 15:21:20 -0900

    dfsg

--- OpenSSL/log_parse.py
+++ OpenSSL/log_parse.py
@@ -132,22 +132,27 @@ def main(infile, outfile):
         f.write(f"Key Exchange: {kx_algo_g}\n")
         f.write(f"Signature: {sg_algo_g}\n")
         f.write(f"Encryption and Hashing: {en_dg_algo_g}\n\n")
-        f.write(f"Total execution time of all direct invocations: {"{:,.2f}".format(total_dur)}\n")
+        #f.write(f"Total execution time of all direct invocations: {"{:,.2f}".format(total_dur)}\n")
+        f.write(f"Total execution time of all direct invocations: {total_dur}\n")
         f.write("\nBreakdown of total execution time (direct invocation).\n")
         for index, (key, value) in enumerate(times_sum.items()):
-            f.write(f"Function name: {key}, time (microseconds): {"{:,.2f}".format(value)}\n")
+            #f.write(f"Function name: {key}, time (microseconds): {"{:,.2f}".format(value)}\n")
+            f.write(f"Function name: {key}, time (microseconds): {value}\n")
         f.write("\nNumber of direct invocations.\n")
         for index, (key, value) in enumerate(times_cnt.items()):
-            f.write(f"Function name: {key}, time (microseconds): {"{:,.2f}".format(value)}\n")
+            #f.write(f"Function name: {key}, time (microseconds): {"{:,.2f}".format(value)}\n")
+            f.write(f"Function name: {key}, time (microseconds): {value}\n")
         f.write("\nExecution time of each direct invocation.\n")
         for index, (key, value) in enumerate(times_mean.items()):
-            f.write(f"Function name: {key}, time (microseconds): {"{:,.2f}".format(value)}\n")
+            #f.write(f"Function name: {key}, time (microseconds): {"{:,.2f}".format(value)}\n")
+            f.write(f"Function name: {key}, time (microseconds): {value}\n")
 
         f.write("\n**************Debug**************\n")
         
         f.write(f"Breakdown of total execution time of {kwd_dbg[0]}.\n")
         for index, (key, value) in enumerate(times_sum_ssl.items()):
-            v = "{:,.2f}".format(value)
+            #v = "{:,.2f}".format(value)
+            v = value
             f.write(f"SSL state: {key}, time (microseconds): {v}\n")
 
         f.write(f"\nList of execution times of {kwd_dbg[0]}\n")
@@ -161,6 +166,7 @@ def main(infile, outfile):
                     value.sort(reverse=True)
                     for i in range(len(value)):
                         value[i] = "%.2f"%value[i]
+
                     f.write(f"\nFunction name: {key}, time (microseconds): {value[:5]}\n")       
 
 if __name__ == "__main__":
